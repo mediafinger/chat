b0679cccacda2832f64f2505d81b95ad8364767b
Andreas Finger <webmaster@mediafinger.com>
2016-02-18 17:41:43 +0100

    Add message content length validation without exception handling

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,11 +2,14 @@ class Message < ApplicationRecord
   belongs_to :user
 
   LIMIT = 5
+  MAXIMUM_MESSAGE_LENGTH = 65535
 
   # use after_create_commit to ensure the Message was written to the DB
   # when using after_create the Job might not find the Message in the DB
   #
   after_create_commit { MessageBroadcastJob.perform_later self }
+
+  validates :content, length: { in: 1..MAXIMUM_MESSAGE_LENGTH }  # TODO handle exceptions & add frontend validation
 
   scope :latest,     -> { limit(LIMIT).order(created_at: :desc) }
   scope :older_than, ->(timestamp) { where("created_at < (?)", timestamp).limit(LIMIT).order(created_at: :desc) }
